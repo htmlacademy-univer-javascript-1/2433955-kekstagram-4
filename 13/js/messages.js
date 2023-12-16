39742a545806ee8279df3c4ab8b4a82929b06520
Keks <keks@htmlacademy.ru>
2023-12-16 13:23:15 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/messages.js
@@ -0,0 +1,59 @@
+import { closeForm } from './form.js';
+import { isEscKey } from './util.js';
+import { uploadData } from './fetch.js';
+
+const errorMessage = document.querySelector('#error').content.querySelector('.error');
+const successMessage = document.querySelector('#success').content.querySelector('.messages');
+const formUpload = document.querySelector('.img-upload__form');
+
+const closePopup = () => {
+  const popup = document.querySelector('.error') || document.querySelector('.success');
+  popup.remove();
+};
+
+const onEscKeydown = (evt) => {
+  if (isEscKey(evt)) {
+    closePopup();
+  }
+};
+
+const onPopupClick = (evt) => {
+  if (!evt.target.classList.contains('success__inner') && !evt.target.classList.contains('error__inner')) {
+    evt.preventDefault();
+    closePopup();
+    document.removeEventListener('keydown', onEscKeydown);
+  }
+};
+
+const showMessage = (message) => {
+  message.addEventListener('click', onPopupClick);
+  document.body.appendChild(message);
+  document.addEventListener('keydown', onEscKeydown, {once: true});
+};
+
+const showErrorMessage = () => {
+  const messageFragment = errorMessage.cloneNode(true);
+  showMessage(messageFragment);
+};
+
+const showSuccessMessage = () => {
+  const messageFragment = successMessage.cloneNode(true);
+  showMessage(messageFragment);
+};
+
+const onSuccess = () => {
+  closeForm();
+  showSuccessMessage();
+};
+
+const onError = () => {
+  showErrorMessage();
+  // closeForm();
+};
+
+const onFormUploadSubmit = (evt) => {
+  evt.preventDefault();
+  uploadData(onSuccess, onError, 'POST', new FormData(evt.target));
+};
+
+formUpload.addEventListener('submit', onFormUploadSubmit);
---
+++ 13/js/util.js
@@ -0,0 +1,72 @@
+const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// 1 функция
+const len = (str, maxLen) => str.length <= maxLen;
+
+len('проверяемая строка', 20);
+
+// 2 функция
+// eslint-disable-next-line no-unused-vars
+const isPalindrom = (str) => {
+  str = str.replaceAll(' ', '').toLowerCase();
+  const leng = str.length - 1;
+  let reversedString = '';
+  for (let i = leng; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  if (str === reversedString) {
+    return true;
+  }
+  return false;
+};
+
+// 3 функция
+const toNumber = (str) => {
+  if (typeof str === Number) {
+    return str;
+  }
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    const currentCharacter = parseInt(str[i], 10);
+    if (!isNaN(currentCharacter)) {
+      result += currentCharacter;
+    }
+  }
+  return result;
+};
+
+const DELAY = 500;
+
+const Keys = {
+  ESCAPE: 'Escape',
+  ESC: 'Esc',
+};
+
+const checkStringLength = (string, length) => string.length <= length;
+
+const debounce = (cb) => {
+  let lastTimeout = null;
+
+  return (...args) => {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(() => {
+      cb(...args);
+    }, DELAY);
+  };
+};
+
+const shuffleArray = (array) => {
+  for (let indexOne = array.length - 1; indexOne > 0; indexOne--) {
+    const indexTwo = Math.floor(Math.random() * (indexOne + 1));
+    [array[indexOne], array[indexTwo]] = [array[indexTwo], array[indexOne]];
+  }
+  return array;
+};
+
+const isEscKey = (evt) => evt.key === Keys.ESCAPE || evt.key === Keys.ESC;
+
+const isButton = (evt) => evt.target.tagName === 'BUTTON';
+
+export {randomInteger, toNumber, isPalindrom, isEscKey, checkStringLength, debounce, shuffleArray, isButton};
